1c73512f970ac0443b4ad1c1762bd26c4f9988c0
catherineisonline <ekaterine.jpg@gmail.com>
2025-08-05 12:20:46 +0700

    Improve width responsiveness and hovers

--- app/components/home/quote/Quote.styled.tsx
+++ app/components/home/quote/Quote.styled.tsx
@@ -4,7 +4,7 @@ import { media } from "../../../styles/breakpoints";
 export const QuoteSection = styled.section`
   text-align: center;
   width: 100%;
-  max-width: 40rem;
+  max-width: 38rem;
   margin: 0 auto;
   display: flex;
   justify-content: center;
@@ -25,14 +25,17 @@ export const QuoteDisplayText = styled.p<{ $bgImage?: boolean }>`
   line-height: 1.3;
   cursor: pointer;
   transition: var(--transition);
-
-  &:hover {
-    color: ${({ theme }) => theme.highlight};
-    transform: var(--hover-transform);
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      color: ${({ theme }) => theme.highlight};
+      transform: var(--hover-transform);
+    }
   }
   &:focus,
   &:active {
+    outline: none;
     color: ${({ theme }) => theme.highlight};
-    border: 1px solid ${({ theme }) => theme.highlight};
+    border-bottom: 2px solid ${({ theme }) => theme.highlight};
+    cursor: auto;
   }
 `;
