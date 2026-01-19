1411050384d5e67f905075e58fff7b4272f634fd
catherineisonline <ekaterine.jpg@gmail.com>
2026-01-19 19:07:00 +0530

    add login, register, profile, selectors

---
+++ app/redux/selectors/loginSelectors.ts
@@ -0,0 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export const selectLoginForm = (state: RootState) => state.login.form;
+export const selectLoginErrors = (state: RootState) => state.login.errors;
+export const selectLoginActiveTab = (state: RootState) => state.login.activeTab;
+export const registerSelectors = createSelector(
+  [selectLoginForm, selectLoginErrors, selectLoginActiveTab],
+  (form, errors, activeTab) => ({
+    form,
+    errors,
+    activeTab,
+  }),
+);
---
+++ app/redux/selectors/profileSelectors.ts
@@ -0,0 +1,24 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export const selectProfileForm = (state: RootState) => state.profile.form;
+export const selectProfileErrors = (state: RootState) => state.profile.errors;
+export const selectProfileUser = (state: RootState) => state.profile.user;
+export const selectProfileIsProfileEditing = (state: RootState) => state.profile.isProfileEditing;
+export const selectProfileIsConfirmationActive = (state: RootState) => state.profile.isConfirmationActive;
+export const registerSelectors = createSelector(
+  [
+    selectProfileForm,
+    selectProfileErrors,
+    selectProfileUser,
+    selectProfileIsProfileEditing,
+    selectProfileIsConfirmationActive,
+  ],
+  (form, errors, user, isProfileEditing, isConfirmationActive) => ({
+    form,
+    errors,
+    user,
+    isProfileEditing,
+    isConfirmationActive,
+  }),
+);
